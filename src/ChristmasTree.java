b8ebe9ba69f5fafd5ee192d3d66b06ff11bff578
tippur.sanchitha <tippur.sanchitha@SEMS-259-13.SEMS259.LAB>
2017-12-20 14:49:54 -0800

    ChristmasTree YAY

--- src/ChristmasTree.java
+++ src/ChristmasTree.java
@@ -5,78 +5,78 @@
  *    
  ******************************************************************************/
 
-import org.jointheleague.graphical.robot.Robot;
+import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class ChristmasTree {
 
-	Robot rob  = new Robot();
-	
-    public static void main(String[] args) {
-        ChristmasTree ohChristmasTree = new ChristmasTree();
-        ohChristmasTree.drawStar();
-        ohChristmasTree.drawTreeBody();
-        ohChristmasTree.drawTreeTrunk();
-    }
+	private static final Color Color = null;
+	Robot rob = new Robot("batman");
+
+	public static void main(String[] args) {
+		ChristmasTree ohChristmasTree = new ChristmasTree();
+		ohChristmasTree.drawStar();
+		ohChristmasTree.drawTreeBody();
+		ohChristmasTree.drawTreeTrunk();
+	}
+
+	double treeWidth = 15;
+	double scale = 1.1; // This is how much the width of the tree increases with each layer down
+
+	void drawTreeBody() {
+		rob.setSpeed(19);
+		// 8. Change the color of the line the robot draws to forest green
+		rob.penDown();
+		rob.setPenColor(Color.CYAN);
+		// 1. Make a variable for turnAmount and set it to 175
+		int turnAmount = 175;
+
+		// 2. Start the Robot facing to the right
+		rob.setAngle(90);
+
+		// 5. Repeat steps 3 through 11, 11 times
+		for (int i = 0; i < 11; i++) {
+
+			// 3. Move the robot the width of the "batman"
+			rob.move((int) treeWidth);
+			// 4. Turn the robot the current turnAmount to the right
+			rob.turn((int) turnAmount);
+			// 6. Set the treeWidth to the current treeWidth times the scale
+			treeWidth = treeWidth * scale;
+			// 7. Move the robot the width of a tree again
+			rob.move((int) treeWidth);
+			// 9. Turn the robot the current turn amount to the LEFT
+			rob.turn((int) -turnAmount);
+			// 10. Set the treeWidth to the current treeWidth times the scale again
+			treeWidth = treeWidth * scale;
+			// 11. Decrease turnAmount by 1
+
+			turnAmount = turnAmount - 1;
+		}
+	}
+
+	void drawTreeTrunk() {
+		// 1. Move the robot half the width of the tree
+rob.move((int) ((int)treeWidth*0.5)); 
+		// 2. Change the robot so that it is pointing straight down
+rob.setAngle(180);
+		// 4. Set the pen width to the tree width divided by 10
+rob.setPenWidth((int) treeWidth/10);
+		// 5. Change the color of the line the robot draws to brown
+rob.setPenColor(java.awt.Color.BLACK);
 
 
-    double treeWidth = 15;
-    double scale = 1.1;    //This is how much the width of the tree increases with each layer down
 
 
-    void drawTreeBody() {
-        // 8. Change the color of the line the robot draws to forest green    
+		// 3. Move the robot a quarter the tree width
+rob.move((int)(treeWidth*0.25));
+	}
 
+	void drawStar() {
+		// * Optional: Draw a red star on top of the tree. Hint: 144 degrees makes a
+		// star.
 
-        // 1. Make a variable for turnAmount and set it to 175
-
-
-        // 2. Start the Robot facing to the right
-
-
-        // 5. Repeat steps 3 through 11, 11 times
-
-
-            // 3. Move the robot the width of the tree
-    
-            // 4. Turn the robot the current turnAmount to the right
-    
-            // 6. Set the treeWidth to the current treeWidth times the scale
-    
-            // 7. Move the robot the width of a tree again
-    
-            // 9. Turn the robot the current turn amount to the LEFT
-    
-            // 10. Set the treeWidth to the current treeWidth times the scale again
-    
-            // 11. Decrease turnAmount by 1
-
+	}
 
 }
-    
-    void drawTreeTrunk() {
-        // 1. Move the robot half the width of the tree
-
-
-        // 2. Change the robot so that it is pointing straight down
-
-
-        // 4. Set the pen width to the tree width divided by 10
-
-
-        // 5. Change the color of the line the robot draws to brown
-
-
-        // 3. Move the robot a quarter the tree width
-
-
-    }
-    
-    void drawStar() {
-        // * Optional: Draw a red star on top of the tree. Hint: 144 degrees makes a star.
-    
-    }
-
-
-}
-
